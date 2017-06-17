5d8f70e6b45d8075b158c567a3ec0a76e401b966
Chris Sheppard <chrissheppard41@gmail.com>
2017-06-17 20:38:52 +0100

    Adding the basic functionality to spider and get the webpage data

--- logistics/src/main/java/com/site/Spider/SpiderController.java
+++ logistics/src/main/java/com/site/Spider/SpiderController.java
@@ -1,20 +1,9 @@
 package com.site.Spider;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import com.site.Spider.Operation.Spider;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sun.misc.IOUtils;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 
 /**
  * @author chrissheppard
@@ -33,38 +22,12 @@ public class SpiderController {
         //save that result as a pass or fail
         //return that result
 
-        StringBuilder sb = new StringBuilder();
-        URL url = null;
-        try {
-            url = new URL("http://127.0.0.1:4000/");
-            URLConnection con = url.openConnection();
+        String website = "http://127.0.0.1:4000";
 
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(
-                            con.getInputStream()
-                    )
-            );
-            in.lines()
-                    .forEach(line -> sb.append(line));
+        Spider spider = new Spider(website);
+        spider.cycle("/", "/");
 
-            in.close();
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        if(sb.length() != 0) {
-            Document doc = Jsoup.parse(sb.toString());
-
-            Elements a = doc.getElementsByTag("a");
-
-            a.stream()
-                    .forEach(link -> {
-                        System.out.println(link.attr("href"));
-                    });
-        }
+        spider.getUrls().forEach((i, k) -> System.out.println(i + " called " + k));
 
         return "Greetings from Spring Boot!";
     }
